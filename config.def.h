9e10b2db8cf16152c093842ef6b3f6357b061822
Eli Peery <eli.g.peery@gmail.com>
2019-06-14 12:55:52 -0400

    config

--- config.def.h
+++ config.def.h
@@ -15,7 +15,7 @@ static const char col_cyan[]        = "#008EC4";
 static const char *colors[][3]      = {
   /*               fg         bg         border   */
   [SchemeNorm] = { col_gray2, col_gray3, col_gray1 },
-  [SchemeSel]  = { col_gray1, col_gray4,  col_gray4  },
+  [SchemeSel]  = { col_gray1, col_gray4,  col_cyan  },
 };
 
 /* tagging */
