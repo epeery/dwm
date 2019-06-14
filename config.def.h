76818f83fe4aceb3d083ec190455ee3ef798746e
Eli Peery <eli.g.peery@gmail.com>
2019-06-14 13:46:23 -0400

    config

--- config.def.h
+++ config.def.h
@@ -28,11 +28,10 @@ static const Rule rules[] = {
    */
   /* class      instance    title       tags mask     isfloating   monitor */
   { "Firefox",  NULL,       NULL,       2,            0,           -1 },
-  { NULL,       NULL,       "Spotify",  9,            0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.61; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
