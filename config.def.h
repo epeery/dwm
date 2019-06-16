8b23f638d2f4c94a16b9d5840cb9a320054d36a2
Eli Peery <eli.g.peery@gmail.com>
2019-06-15 20:26:40 -0400

    config

--- config.def.h
+++ config.def.h
@@ -58,7 +58,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xst", NULL };
 static const char *browser[]  = { "firefox-developer-edition", NULL };
-static const char *filemanager[]  = { "xst ", "-e", "ranger" };
+static const char *filemanager[]  = { "xst ", "-e ranger" };
 
 #include "movestack.c"
 
