a4588b0e029adaad06e48fe90866f99dcb5601e1
Eli Peery <eli.g.peery@gmail.com>
2019-06-15 20:21:11 -0400

    config

--- config.def.h
+++ config.def.h
@@ -58,6 +58,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xst", NULL };
 static const char *browser[]  = { "firefox-developer-edition", NULL };
+static const char *filemanager[]  = { "ranger", NULL };
 
 #include "movestack.c"
 
@@ -66,6 +67,7 @@ static Key keys[] = {
   { MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
   { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
   { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = browser } },
+  { MODKEY,                       XK_r, spawn,          {.v = filemanager } },
   { MODKEY,                       XK_b,      togglebar,      {0} },
   { MODKEY,                       XK_s,      swapfocus,     {0} },
   { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
